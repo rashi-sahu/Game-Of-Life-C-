293fcda4b4d183fb45173f20a3d25a5be8fd7639
Rashi Sahu <v-rsahu@microsoft.com>
2021-01-10 15:33:23 +0530

    Calculated the next generation grid

--- GameOfLife/Grid.cs
+++ GameOfLife/Grid.cs
@@ -41,6 +41,10 @@ namespace GameOfLife
         public List<List<int>> NextGeneration()
         {
             this.grid = ExpandGrid();
+            this.row = this.grid.Count;
+            this.column = this.grid[0].Count;
+            List<List<int>> gridCopy = GenerateDeepCopyOfGrid();
+            this.grid = CalculateNextGenerationCells(gridCopy);
 
             return this.grid;
         }
@@ -74,5 +78,73 @@ namespace GameOfLife
                 return new List<List<int>>();
             }
         }
-     }
+
+        private List<List<int>> GenerateDeepCopyOfGrid()
+        {
+            List<List<int>> gridCopy = new List<List<int>>();
+            for (int index = 0; index < grid.Count; index++)
+            {
+                gridCopy.Add(new List<int>(this.grid[index]));
+            }
+            return gridCopy;
+        }
+
+        private List<List<int>> CalculateNextGenerationCells(List<List<int>> gridCopy)
+        {
+            for (int l = 0; l < this.row; l++)
+            {
+                for (int m = 0; m < this.column; m++)
+                {
+                    int aliveNeighbours = CountAliveNeighbours(gridCopy, l, m);
+                    if ((gridCopy[l][m] == 1) && (aliveNeighbours < 2))
+                        grid[l][m] = 0;
+
+                    else if ((gridCopy[l][m] == 1) && (aliveNeighbours > 3))
+                        grid[l][m] = 0;
+
+                    else if ((gridCopy[l][m] == 0) && (aliveNeighbours == 3))
+                        grid[l][m] = 1;
+
+                    else
+                        grid[l][m] = gridCopy[l][m];
+                }
+            }
+
+            return grid;
+        }
+
+        private int CountAliveNeighbours(List<List<int>> gridCopy, int x, int y)
+        {
+            int aliveNeighbours = 0;
+
+            if (this.row > 0)
+            {
+                for (int i = -1; i <= 1; i++)
+                {
+                    for (int j = -1; j <= 1; j++)
+                    {
+                        if (CheckValidCell(x + i, y + j))
+                        {
+                            aliveNeighbours += gridCopy[x + i][y + j];
+                        }
+                    }
+                }
+                aliveNeighbours -= gridCopy[x][y];
+            }
+
+            return aliveNeighbours;
+        }
+
+        private bool CheckValidCell(int x, int y)
+        {
+            if (x >= 0 && x < this.row && y >= 0 && y < this.column)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
 }
